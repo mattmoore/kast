ef5422612b6a5a74ab8114ce14c2e6c704e898d7
Matt Moore <mattmoore@carbonhelix.com>
2019-07-14 23:35:04 +0000

    Build scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("com.gradle.build-scan").version("2.1")
 }
 
 repositories {
@@ -11,4 +12,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.21")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    // Use Monocle for headless testfx
+    testCompile("org.testfx:openjfx-monocle:8u76-b04")
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
 }
