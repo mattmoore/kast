48057297e60c736a2ed658cfb0b0818b3173a596
Matt Moore <mattmoore@carbonhelix.com>
2019-07-16 02:07:28 +0000

    Add TornadoFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.21")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    compile("no.tornado:tornadofx:1.7.19")
     // Use Monocle for headless testfx
     testCompile("org.testfx:openjfx-monocle:8u76-b04")
 }
