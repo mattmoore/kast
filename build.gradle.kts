215a9d2c47a83e78edcef0f3660482f79f581ff4
Matt Moore <mattmoore@carbonhelix.com>
2019-07-31 16:42:55 -0500

    Add support for block expressions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+    id("org.jetbrains.kotlin.jvm").version("1.3.41")
     id("com.gradle.build-scan").version("2.1")
 }
 
@@ -9,12 +11,18 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.21")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.41")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     compile("no.tornado:tornadofx:1.7.19")
     // Use Monocle for headless testfx
     testCompile("org.testfx:openjfx-monocle:8u76-b04")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 buildScan {
